32defca152c4a517fb691fb8986028b6660d7413
shilpabn1 <161477625+shilpabn1@users.noreply.github.com>
2024-02-27 23:50:36 +0530

    add

---
+++ LearnJumpStatements.java
@@ -0,0 +1,31 @@
+package week1.day1;
+
+public class LearnJumpStatements {
+	public static void main(String[] args) {
+		
+		        //1,2,3,4,6,8,9,10
+				//1,2,3,4
+		for(int i=1; i<=10; i++) {
+			if((i==5) || (i==6)) {
+				continue;    // continue keyword is used to skip the current iteration
+			} if(i==6) {
+				break;       // break keyword will stop the entire iteration
+			}
+		 {
+			System.out.println(i);
+		}
+		
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+
+}
